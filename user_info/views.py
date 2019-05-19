524b1e843c253f602497cdb38fd1126a55dd4597
clhiker <1911618290@qq.com>
2019-05-19 17:43:49 +0800

    Front-end and back-end interaction through JSON

---
+++ user_info/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, render_to_response
+from django.http import JsonResponse
+
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import UserInfo
+# Create your views here.
+def hello(request):
+    return JsonResponse({'result': 200, 'msg': '连接成功'})
+
+def index(request):
+    # return HttpResponse('welcome to style change !')
+    return render_to_response('index.html')
+
+
+@csrf_exempt #屏蔽装饰器器
+def registerApi(request):
+    if request.method == 'POST':
+        req = json.loads(request.body) #取得数据
+        userID = req['userID']
+        pwd = req['pwd']
+        searchArray = UserInfo.objects.get_or_create(username=userID) #尝试创建用户
+        print(searchArray)
+        if searchArray[1] == True:
+            return JsonResponse({'result': 200, 'msg':'注册成功'})
+        else:
+            return JsonResponse({'result': 200, 'msg':'已有重复用户名'})
