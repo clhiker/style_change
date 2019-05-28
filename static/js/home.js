04bf57240065e267d7601904650c61787ef4857e
lmqwer <35553393+lmqwer@users.noreply.github.com>
2019-05-28 15:40:55 +0800

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -1,28 +1,68 @@
-// function changepic() {
-//     var reads = new FileReader();
-//     f = document.getElementById('file').files[0];
-//     reads.readAsDataURL(f);
-//     reads.onload = function(e) {
-//         document.getElementById('img3').src = this.result;
-//         $("#img3").css("display", "block");
-//     };
-// }
-//
+//风格图片数组
+var imgs = new Array("../static/image/example.jpg","../static/image/bg.jpg","../static/image/black.jpg")
+var i = 0;
+//向左滑动一张图片
+function leftChange() {
+    if(i<imgs.length-1){
+        i++;
+        Id("style_image").src=imgs[i];
+    }
+}
+//向右滑动一张图片
+function rightChange() {
+    if(i>0){
+        i--;
+        Id("style_image").src=imgs[i];
+    }
+}
+//获得文件对象引用
+function Id(id){
+    return document.getElementById(id);
+}
+//选择图片
+function changeToop(){
+    var file = Id("file");
+    if(file.value==''){
+        //透明度设置为0
+        Id("image").style.opacity=0;
+    }else{
+        preImg("file","image");
+        //透明度设置为1
+        Id("image").style.opacity=1;
+    }
+}
+//获取input[file]图片的url
+function getFileUrl(fileId) {
+    var url;
+    var file = Id(fileId);
+    var agent = navigator.userAgent;
+    if (agent.indexOf("MSIE")>=1) {
+        url = file.value;
+    } else if(agent.indexOf("Firefox")>0) {
+        url = window.URL.createObjectURL(file.files.item(0));
+    } else if(agent.indexOf("Chrome")>0) {
+        url = window.URL.createObjectURL(file.files.item(0));
+    }
+    return url;
+}
+//读取图片后预览
+function preImg(fileId,imgId) {
+    var imgPre =Id(imgId);
+    imgPre.src = getFileUrl(fileId);
+}
 
-// 开始转换
-function beginChange() {
-    $.ajax({
-        url : "/beginChange",
-        contentType: 'application/json',
-        type: 'POST',
-        data: 'begin_change',
-        success: function (result) {
-            console.log('成功');
-        },
-        fail: function (result) {
-          console.log('失败');
-        }
-    });
+//风格转换
+function stylechange() {
+    var cg_img = Id("change_image")
+    var file = Id("file");
+    if(file.value==''){
+        //透明度设置为0
+        Id("change_image").style.opacity=0;
+    }else{
+        cg_img.src=getFileUrl("file");
+        //透明度设置为1
+        Id("change_image").style.opacity=1;
+    }
 }
 
 function uploadFile() {
