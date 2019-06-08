b2b1fefa1b31458a326b164008f0c26b7b4d1bdc
lmqwer <35553393+lmqwer@users.noreply.github.com>
2019-06-08 17:36:43 +0800

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -1,34 +1,49 @@
 //风格图片数组
-var imgs = new Array("../static/image/example.jpg","../static/image/style1.jpg","../static/image/style2.jpg","../static/image/style3.jpg");
+var imgs = new Array("../static/image/example.jpg","../static/image/style1.jpg","../static/image/style2.jpg","../static/image/style3.jpg")
 var i = 0;
-//向左滑动一张图片
+
 function leftChange() {
     if(i<imgs.length-1){
         i++;
         Id("style_image").src=imgs[i];
     }
 }
-//向右滑动一张图片
+
 function rightChange() {
     if(i>0){
         i--;
         Id("style_image").src=imgs[i];
     }
 }
-//获得文件对象引用
+//在风格图片数组中选择风格图片
+function selectStyle() {
+    var temp = Id("style_image");
+    Id("image2").src = temp.src;
+}
+
 function Id(id){
     return document.getElementById(id);
 }
-//选择图片
-function changeToop(){
-    var file = Id("file");
+//选择原图
+function changeImage(){
+    var img = Id("image1")
+    var file = Id("file1");
     if(file.value==''){
-        //透明度设置为0
-        Id("image").style.opacity=0;
+        //设置默认图片
+        img.src='../static/image/upload1.png';
     }else{
-        preImg("file","image");
-        //透明度设置为1
-        Id("image").style.opacity=1;
+        preImg("file1","image1");
+    }
+}
+//选择自定义风格图片
+function changeStyle() {
+    var img = Id("image2")
+    var file = Id("file2");
+    if (file.value == '') {
+        //设置默认图片
+        img.src = '../static/image/upload1.png';
+    } else {
+        preImg("file2", "image2");
     }
 }
 //获取input[file]图片的url
@@ -52,16 +67,19 @@ function preImg(fileId,imgId) {
 }
 
 //风格转换
-function stylechange() {
+function styleChange() {
     var cg_img = Id("change_image")
-    var file = Id("file");
-    if(file.value==''){
-        //透明度设置为0
-        Id("change_image").style.opacity=0;
+    var file1 = Id("file1");
+    var file2 = Id("file2");
+    console.log(file1.value);
+    console.log(file2.value);
+    if(file1.value==''||file2.value==''){
+        // //设置默认图片
+        cg_img.src='../static/image/upload1.png';
     }else{
-        cg_img.src=getFileUrl("file");
-        //透明度设置为1
-        Id("change_image").style.opacity=1;
+        //风格转换后的图片
+        cg_img.src=getFileUrl("file1");
+        // Id("change_image").style.opacity=1;
     }
 }
 
